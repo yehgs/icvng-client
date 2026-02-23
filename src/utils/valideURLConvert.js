f85946c2d938e14f1762e2cc39504a1fef3cee33
Caleb Opule <opulecalebtin@gmail.com>
2026-02-23 08:35:42 +0100

    updated pricing display2

--- src/utils/valideURLConvert.js
+++ src/utils/valideURLConvert.js
@@ -1,16 +1,17 @@
+//icvng-client/src/utils/valideURLConvert.js
 export const valideURLConvert = (name) => {
   const url = name
     ?.toString()
-    .replaceAll(' ', '-')
-    .replaceAll(',', '-')
-    .replaceAll('&', '-')
-    .replaceAll('/', '-') // Fix for forward slashes
-    .replaceAll('\\', '-') // Fix for backslashes
-    .replaceAll('%', '-') // Fix for percentage signs
-    .replaceAll('+', '-') // Fix for plus signs
-    .replaceAll('(', '-') // Fix for parentheses
-    .replaceAll(')', '-') // Fix for parentheses
-    .replaceAll('--', '-') // Replace double hyphens with single
+    .replaceAll(" ", "-")
+    .replaceAll(",", "-")
+    .replaceAll("&", "-")
+    .replaceAll("/", "-") // Fix for forward slashes
+    .replaceAll("\\", "-") // Fix for backslashes
+    .replaceAll("%", "-") // Fix for percentage signs
+    .replaceAll("+", "-") // Fix for plus signs
+    .replaceAll("(", "-") // Fix for parentheses
+    .replaceAll(")", "-") // Fix for parentheses
+    .replaceAll("--", "-") // Replace double hyphens with single
     .toLowerCase();
 
   return url;
